b4daed1f3593f70d0974d4ff9936430949012509
Matthew Shultz <matthew.shultz@yale.edu>
2015-04-16 14:28:14 -0400

    Included some bison dynamics

--- rangeland.R
+++ rangeland.R
@@ -15,19 +15,28 @@ library(deSolve)
 # eC = cattle biomass conversion efficiency
 # dhC = rate of cattle harvest by humans
 
+# Bison
+# B0 = initial densiry of bison within entire space
+# rb = bison intrinsic growth rate = .22 / year = .018 / month
+# dhB = rate of bison culling by humans
+# tB = proportion of time bison spend in range areas near water
+
 model = function(t, y, parms) {
   
   Gw = y[1]
   Gd = y[2]
   C = y[3]
+  B = y[4]
   
   with(as.list(params), {
     
-    dGw = Gw * rG * ( 1 - Gw / kG ) - Gw * aC * C / w2t
-    dGd = Gd * rG * ( 1 - Gw / kG )
+    dGw = Gw * rG * ( 1 - Gw / kG ) - Gw * ( aC * C / w2t + tB * B)
+    dGd = Gd * rG * ( 1 - Gd / kG ) - Gd * (1 - tB) * aB * B
     dC  = eC * aC * C * Gw * w2t - dhC * C
+    dB  = rB * B - dhB * B
     
-    differentials = c(dGw, dGd, dC)
+    # [ (1 - tB) * Gd + tB * Gw ] 
+    differentials = c(dGw, dGd, dC, dB)
     list(differentials)
   
   })
@@ -39,28 +48,36 @@ params = c(
         aC = .01,  # from stocking rates whitepapers?
         eC = .1,
         dhC = .15,
-        kG = 1000
+        kG = 10000,
+        aB = .01,  # unused
+        eB = .1,   # unused
+        rB = .018,  # intrinsic growth rate
+        dhB = .012, # culling rate, allowing some growth
+        tB = .2  # this can from that paper
         )
 
-Gw0 = 1000
+Gw0 = 600
 Gd0 = 1000
-C0 = 10
+C0 = 15
+B0 = 0
   
 state = c(
           Gw0,
           Gd0,
-          C0
+          C0,
+          B0
         )
 
 
-t = 1:100  # months ??
+t = 1:200  # months ??
 
 out = ode(y = state, times = t, func = model, parms = params)
 
-par(mfrow=c(1,3))
+par(mfrow=c(2,2))
 matplot(t, out[,2], type = "l", ylab = "Grass near water", xlab = 'months')
 matplot(t, out[,3], type = "l", ylab = "Grass not near water", xlab = 'months')
 matplot(t, out[,4], type = "l", ylab = "Cattle", xlab = 'months', ylim=c(0, max(out[,4])))
+matplot(t, out[,5], type = "l", ylab = "Bison", xlab = 'months', ylim=c(0, max(out[,5])))
 par(mfrow=c(1,1))
 
 
